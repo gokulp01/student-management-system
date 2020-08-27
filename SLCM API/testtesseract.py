7791c5afa7599ff685c51136682b11474a7347c7
KARTHIK R <karthikramakrishnan14@gmail.com>
2020-08-27 18:53:04 +0530

    removed unused files

--- SLCM API/testtesseract.py
+++
@@ -1,8 +0,0 @@
-import pytesseract as tess
-from PIL import Image
-tess.pytesseract.tesseract_cmd = r'D:\TESSERACT\tesseract.exe'
-img = Image.open('GenerateCaptcha.jpg')
-img = img.convert('L')
-img.save('final.jpg')
-text = tess.image_to_string(img)
-print(text)
