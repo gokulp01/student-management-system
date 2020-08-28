ed5801fc9936adaed542bb536faedd4c47f6f1c0
gokul <gokulgns@gmail.com>
2020-08-28 14:06:48 +0530

    Add firefox support

--- chrome/getcaptchaimg.py
+++ chrome/getcaptchaimg.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+# from slcm import user_name, pass_word
 import requests
 import urllib
 import time
@@ -21,37 +22,44 @@ headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 baseurl = 'https://slcm.manipal.edu/'
 url ="https://slcm.manipal.edu/loginForm.aspx"
 resp = requests.get(url,headers=headers)
-s = BeautifulSoup(resp.content,features="lxml")
-images = s.find('img',{'id':'imgCaptcha'})['src']
 
-captchaurl = baseurl + images 
-print(captchaurl)
-##op = webdriver.ChromeOptions()
-##op.add_argument('headless')
-driver = webdriver.Chrome(ChromeDriverManager().install()) ##,options=op
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+def get_capt():
+    s = BeautifulSoup(resp.content,features="lxml")
+    images = s.find('img',{'id':'imgCaptcha'})['src']
+    captchaurl = baseurl + images 
+    print(captchaurl)
+    driver.execute_script("window.open()")
+    driver.switch_to_window(driver.window_handles[1])
+    driver.get(captchaurl)
+    time.sleep(8)
+    element = driver.find_element_by_xpath('/html/body/img') # find part of the page you want image of
+    location = element.location
+    size = element.size
+    driver.save_screenshot('screenshot.png')
+    im = Image.open('screenshot.png') # uses PIL library to open image in memory
+    left = location['x']
+    top = location['y']
+    right =location['x'] + size['width']
+    bottom = location['y'] + size['height']
+    im = im.crop((left, top, right, bottom))
+    im = im.convert('L') # defines crop points
+    im.save('screenshot.png')
+    img = Image.open('screenshot.png')
+    captcha_text = tess.image_to_string(img).strip()
+    print(captcha_text)
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    return captcha_text
+
 driver.get(url)
 driver.implicitly_wait(3)
-driver.execute_script("window.open()")
-driver.switch_to_window(driver.window_handles[1])
-driver.get(captchaurl)
-time.sleep(8)
-element = driver.find_element_by_xpath('/html/body/img') # find part of the page you want image of
-location = element.location
-size = element.size
-driver.save_screenshot('screenshot.png')
-im = Image.open('screenshot.png') # uses PIL library to open image in memory
-left = location['x']
-top = location['y']
-right =location['x'] + size['width']
-bottom = location['y'] + size['height']
-im = im.crop((left, top, right, bottom))
-im = im.convert('L') # defines crop points
-im.save('screenshot.png')
-img = Image.open('screenshot.png')
-captcha_text = tess.image_to_string(img).strip()
-print(captcha_text)
-driver.switch_to.window(driver.window_handles[0])
+
+
+
 def login_to_website(username=None,password=None):
+    captcha_text=get_capt()
     MarksHTML = None
     AttendanceHTML = None
     user_id = driver.find_element_by_xpath('//*[@id="txtUserid"]')
@@ -68,21 +76,17 @@ def login_to_website(username=None,password=None):
     driver.find_element_by_xpath('//*[@id="btnLogin"]').click() 
     try:
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'rtpchkMenu_lnkbtn2_1')))
+        element.click()
     except:
-        print("Invalid Username/Password")
-        driver.close()
-        return None,None
+        print("Invalid Username/Password/Captcha")
+        login_to_website(username, password)
+        
     print ("Logged In")
-    element.click()
     WebDriverWait(driver, 5)
      #Find Attendance Table 
     driver.find_element_by_xpath('//a[@href="#3"]').click();
     element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'tblAttendancePercentage')))
     AttendanceHTML = element.get_attribute('outerHTML')
-    #print(outer)
-    #Find Internal Mark Table
-    #print(marks)
-    #Logout of SLCM and close chromewebdriver
     driver.get("https://slcm.manipal.edu/loginForm.aspx")
     driver.quit()
     return AttendanceHTML
@@ -90,7 +94,3 @@ def login_to_website(username=None,password=None):
 if __name__ == "__main__":
     AttendanceHTML = login_to_website("username","password")
 
-# response = requests.get('https://slcm.manipal.edu/images/logo.png')
-# file = open("captchaimage.jpg", "wb")
-# file.write(response.content)
-# file.close()
